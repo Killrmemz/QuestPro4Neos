4c02dffa992c1b531628be4f8ac87dd26f4d3acb
Memz <yackar@aim.com>
2023-09-17 00:48:41 +0000

    Update FbMessage.cs

--- Interface/FbMessage.cs
+++ Interface/FbMessage.cs
@@ -1,4 +1,4 @@
-﻿using OscCore;
+using OscCore;
 using System;
 
 namespace QuestProModule;
@@ -64,60 +64,6 @@ public class FbMessage
     double yawR = 180.0 / Math.PI * yaw;
 
     // Face Expressions
-
-    // Eyelid edge case, eyes are actually closed now
-    if (FloatNear(Expressions[FbExpression.Eyes_Look_Down_L], Expressions[FbExpression.Eyes_Look_Up_L]) &&
-        Expressions[FbExpression.Eyes_Closed_L] > 0.25f)
-    {
-      Expressions[FbExpression.Eyes_Closed_L] = 0; // 0.9f - (expressions[FBExpression.Lid_Tightener_L] * 3);
-    }
-    else
-    {
-      Expressions[FbExpression.Eyes_Closed_L] = 0.9f - Expressions[FbExpression.Eyes_Closed_L] * 3 /
-        (1 + Expressions[FbExpression.Eyes_Look_Down_L] * 3);
-    }
-
-    // Another eyelid edge case
-    if (FloatNear(Expressions[FbExpression.Eyes_Look_Down_R], Expressions[FbExpression.Eyes_Look_Up_R]) &&
-        Expressions[FbExpression.Eyes_Closed_R] > 0.25f)
-    {
-      Expressions[FbExpression.Eyes_Closed_R] = 0; // 0.9f - (expressions[FBExpression.Lid_Tightener_R] * 3);
-    }
-    else
-    {
-      Expressions[FbExpression.Eyes_Closed_R] = 0.9f - Expressions[FbExpression.Eyes_Closed_R] * 3 /
-        (1 + Expressions[FbExpression.Eyes_Look_Down_R] * 3);
-    }
-
-    //expressions[FBExpression.Lid_Tightener_L = 0.8f-expressions[FBExpression.Eyes_Closed_L]; // Sad: fix combined param instead
-    //expressions[FBExpression.Lid_Tightener_R = 0.8f-expressions[FBExpression.Eyes_Closed_R]; // Sad: fix combined param instead
-
-    if (1 - Expressions[FbExpression.Eyes_Closed_L] < Expressions[FbExpression.Lid_Tightener_L])
-      Expressions[FbExpression.Lid_Tightener_L] = 1 - Expressions[FbExpression.Eyes_Closed_L] - 0.01f;
-
-    if (1 - Expressions[FbExpression.Eyes_Closed_R] < Expressions[FbExpression.Lid_Tightener_R])
-      Expressions[FbExpression.Lid_Tightener_R] = 1 - Expressions[FbExpression.Eyes_Closed_R] - 0.01f;
-
-    //expressions[FBExpression.Lid_Tightener_L = Math.Max(0, expressions[FBExpression.Lid_Tightener_L] - 0.15f);
-    //expressions[FBExpression.Lid_Tightener_R = Math.Max(0, expressions[FBExpression.Lid_Tightener_R] - 0.15f);
-
-    Expressions[FbExpression.Upper_Lid_Raiser_L] = Math.Max(0, Expressions[FbExpression.Upper_Lid_Raiser_L] - 0.5f);
-    Expressions[FbExpression.Upper_Lid_Raiser_R] = Math.Max(0, Expressions[FbExpression.Upper_Lid_Raiser_R] - 0.5f);
-
-    Expressions[FbExpression.Lid_Tightener_L] = Math.Max(0, Expressions[FbExpression.Lid_Tightener_L] - 0.5f);
-    Expressions[FbExpression.Lid_Tightener_R] = Math.Max(0, Expressions[FbExpression.Lid_Tightener_R] - 0.5f);
-
-    Expressions[FbExpression.Inner_Brow_Raiser_L] =
-      Math.Min(1, Expressions[FbExpression.Inner_Brow_Raiser_L] * 3f); // * 4;
-    Expressions[FbExpression.Brow_Lowerer_L] = Math.Min(1, Expressions[FbExpression.Brow_Lowerer_L] * 3f); // * 4;
-    Expressions[FbExpression.Outer_Brow_Raiser_L] =
-      Math.Min(1, Expressions[FbExpression.Outer_Brow_Raiser_L] * 3f); // * 4;
-
-    Expressions[FbExpression.Inner_Brow_Raiser_R] =
-      Math.Min(1, Expressions[FbExpression.Inner_Brow_Raiser_R] * 3f); // * 4;
-    Expressions[FbExpression.Brow_Lowerer_R] = Math.Min(1, Expressions[FbExpression.Brow_Lowerer_R] * 3f); // * 4;
-    Expressions[FbExpression.Outer_Brow_Raiser_R] =
-      Math.Min(1, Expressions[FbExpression.Outer_Brow_Raiser_R] * 3f); // * 4;
 
     Expressions[FbExpression.Eyes_Look_Up_L] *= 0.55f;
     Expressions[FbExpression.Eyes_Look_Up_R] *= 0.55f;
